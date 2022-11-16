5e1bb9ca9462e874ae40128891c805550660a5c3
Helder Fonseca <helder2391@gmail.com>
2022-11-16 11:29:00 -0100

    remove mailerlite popup

--- components/About.js
+++ components/About.js
@@ -1,6 +1,25 @@
 export default function About() {
     return(
-        <section className="mt-32 flex flex-col justify-center items-center mx-auto gap-16 md:w-3/4 w-full">
+
+        <>
+            <section className="mt-32 flex flex-col justify-center items-center mx-auto md:w-1/2 w-full">
+                <h1 className="px-16 md:px-0 text-5xl font-bold md:inline-block p-2 rounded-md text-[#070707] text-center">E-mail Marketing</h1>
+                <h2 className="px-16 md:px-0 text-4xl md:text-center italic font-light pt-3">Faça o seu negócio chegar a <span className="font-bold">mais de 4000 clientes</span> com um único email!</h2>
+                <div className="pt-8 px-16 md:px-0">
+                        <p className="leading-relaxed text-justify">
+                            O marketing por email é o uso do email para promover
+                            produtos ou serviços. Mas não se limita apenas a isso.
+                            Você pode usar o marketing por e-mail para desenvolver
+                            relações com os clientes atuais e / ou para chegar a
+                            clientes em potencial. O marketing por email permite que
+                            as empresas mantenham seus clientes informados e
+                            personalizem as suas mensagens de marketing. Veja nisso
+                            como uma das formas de marketing digital mais
+                            económicas e ricas em conversões da atualidade.
+                        </p>
+                </div>
+            </section>
+            <section className="mt-32 flex flex-col justify-center items-center mx-auto gap-16 md:w-3/4 w-full">
             <h3 className="text-5xl font-bold text-center md:w-[56%] px-8">
                 O que o email marketing faz pela sua empresa?</h3>
 
@@ -16,5 +35,7 @@ export default function About() {
                 </p>
             </div>
         </section>
+        </>
+        
     );
 }
--- components/Hero.js
+++ components/Hero.js
@@ -1,17 +1,20 @@
 import Image from "next/image";
+import ContactForm from "./ContactForm";
+import heroImage from '/public/black-friday.png'
 
 export default function Hero() {
 
-    const heroImage = '/black-friday.png';
+    //const heroImage = '/black-friday.png';
 
     return(
         <div className="mx-auto max-w-6xl">
-            <div className="flex md:flex-row flex-col-reverse gap-16 md:mt-20 text-[#f8f8ff]">
-                <div className="md:w-1/2 w-full">
-                    <Image src={heroImage} width={1090} height={1000} alt="Black Friday"/>
+            <div className="flex md:flex-row flex-col-reverse mt-16 gap-4 md:gap-18 text-[#f8f8ff] items-center">
+                <div className="md:w-1/2 w-full self-center items-center pb-10 md:pb-0">
+                    
+                <ContactForm />
                 </div>
-                <div className="mt-[4rem] md:w-1/2 w-full">
-                    <h1 className="text-5xl font-bold bg-[#FBED04] md:inline-block p-2 rounded-md text-[#070707]">E-mail Marketing</h1>
+                <div className="md:w-1/2 w-full self-center">
+                    {/*<h1 className="text-5xl font-bold bg-[#FBED04] md:inline-block p-2 rounded-md text-[#070707]">E-mail Marketing</h1>
                     <h2 className="text-4xl md:text-left italic font-light pt-3">Faça o seu negócio chegar a <span className="font-bold">mais de 4000 clientes</span> com um único email!</h2>
                     <div className="pt-8">
                         <p className="leading-relaxed text-justify">
@@ -25,7 +28,8 @@ export default function Hero() {
                             como uma das formas de marketing digital mais
                             económicas e ricas em conversões da atualidade.
                         </p>
-                    </div>
+                </div>*/}
+                    <Image src={heroImage} alt="Black Friday" className="object-contain" layout="responsive" priority="true" as="image"/>
                 </div>
             </div>
         </div>
