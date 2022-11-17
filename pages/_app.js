148c04e986d4c31e7dd4e9513478d2b6395a3a34
Helder Fonseca <helder2391@gmail.com>
2022-11-17 03:58:19 -0100

    change page title

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ function MyApp({ Component, pageProps }) {
   return (
           <>
             <Head>
-              <title>Email Marketing</title>
+              <title>Email Marketing - Timor Leste</title>
             </Head>
             <Component {...pageProps} />
           </>
